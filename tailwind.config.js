ee7d8ebcb267031733bb4796d955ac1319d7e11c
Ibranista <ibranista88@gmail.com>
2023-01-03 10:24:32 +0100

    set up tailwindcss.

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/*.js",
+    "./pages/**/*.js",
+    "./components/*.js",
+    "./components/**/*.js",
+    "./pages/**/*.js",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./pages/*{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js",
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
+    "./node_modules/flowbite-react/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
